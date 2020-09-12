d0cfe010c32729f481eca608d50e67815527c7ca
Ibodwe <chlwjddn9181@naver.com>
2020-09-12 07:57:54 +0000

    update cognito config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_s6BRB8Dmi', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6nrb35i37hhrd442q3284u19bp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3ssh7i0nptb1u7bab3n66f8jdk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
